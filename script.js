2294b3bf685989dcdbd42bee830725ba72b22af3
Shannon406 <shannondidlick95@gmail.com>
2024-12-24 21:22:52 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// Open the Google Maps link
+// Open the Google Maps Street View link
 function openMap() {
-  const mapUrl = "https://www.google.com/maps?q=50.38972,-3.51278";
+  const mapUrl = "https://www.google.co.uk/maps/@50.6802413,-3.4638177,3a,75y,224.53h,90.68t/data=!3m7!1e1!3m5!1s8CI9qnmr1qj_YlGSCiOR8Q!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fcb_client%3Dmaps_sv.tactile%26w%3D900%26h%3D600%26pitch%3D-0.6843907659802397%26panoid%3D8CI9qnmr1qj_YlGSCiOR8Q%26yaw%3D224.52612833439517!7i16384!8i8192?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D";
   window.open(mapUrl, "_blank");
 }
